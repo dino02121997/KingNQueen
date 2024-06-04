572060a941bc1afcc24ec7c5159f681d04c3b2ad
dino02121997 <dino02121997@gmail.com>
2024-06-04 23:54:08 +0700

    feat: renderCard function

---
+++ assets/Constant/Common.ts
@@ -0,0 +1,36 @@
+export const SUIT = {
+    SPADE : '♠',
+    HEART : '♥',
+    CLUB : '♣',
+    DIAMOND : '♦'
+}
+export const POINT = {
+    ACE : 1,
+    TWO : 2,
+    THREE : 3,
+    FOUR : 4,
+    FIVE : 5,
+    SIX : 6,
+    SEVEN : 7,
+    EIGHT : 8,
+    NINE : 9,
+    TEN : 10,
+    JACK : 11,
+    QUEEN : 12,
+    KING : 13
+}
+
+export const POINT_LABEL = {
+    2 : "2",
+    3 : "3",
+    4 : "4",
+    5 : "5",
+    6 : "6",
+    7 : "7",
+    8 : "8",
+    9 : "9",
+    10 : "10",
+    11 : "J",
+    12 : "Q",
+    13 : "K"
+}
--- assets/Script/Card.ts
+++ assets/Script/Card.ts
@@ -5,23 +5,50 @@
 // Learn life-cycle callbacks:
 //  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
 
+import { POINT, POINT_LABEL, SUIT } from "../Constant/Common";
+
 const {ccclass, property} = cc._decorator;
 
 @ccclass
-export default class NewClass extends cc.Component {
+export default class Card extends cc.Component {
 
     @property(cc.Node)
     font: cc.Node = null;
 
     @property(cc.Node)
     back: cc.Node = null;
+
+    @property(cc.Label)
+    pointLabel: cc.Label = null;
+
+    @property(cc.Label)
+    suitLabel: cc.Label = null;
+    
   
+    point: number = POINT.ACE;
+    suit: string = SUIT.CLUB
+
     isUpSide = false;
+
+    start () {
+        cc.log("rendering");
+        this.renderCard();
+    }
+    renderCard() {
+        
+        this.pointLabel.string = this.point === POINT.ACE ? this.suit : POINT_LABEL[this.point];
+        this.suitLabel.string = this.point === POINT.ACE ? '' : this.suit;
+
+        if(this.suit === SUIT.CLUB || this.suit === SUIT.SPADE){
+            this.pointLabel.node.color = cc.Color.BLACK;
+            this.suitLabel.node.color = cc.Color.BLACK;
+        }
+    }
 
     flipCard() {
         this.isUpSide =!this.isUpSide;
-        console.log("flipCard");
-        if(this.isUpSide){ // flip to back
+        if(this.isUpSide){ 
+            // flip to back
             cc.tween(this.font).to(0.25, {
                 scaleX:0
             }).start();
@@ -30,7 +57,8 @@ export default class NewClass extends cc.Component {
                 scaleX: 1
             }).start();
         }
-        else{ //flip to front
+        else{ 
+            //flip to front
             cc.tween(this.back).to(0.25, {
                 scaleX: 0
             }).start();
